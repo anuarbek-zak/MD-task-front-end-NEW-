172ef50fe8b03771a45bfef6633d67d85d2c709e
Anuarbek Zakiryanov <anuarbekzakirianov97@gmail.com>
2016-11-22 13:10:16 +0600

    comments

--- src/app/taskInfo/taskInfo.controller.js
+++ src/app/taskInfo/taskInfo.controller.js
@@ -1,11 +1,12 @@
 export class TaskInfoController{
-    constructor($http, $localStorage,$stateParams, CheckAuthService, envService){
+    constructor($http, $localStorage,$stateParams,toastr, CheckAuthService, envService){
         'ngInject';
         var vm = this;
         let userId = $localStorage.user._id;
+        vm.userId = userId;
         vm.taskId = $stateParams.taskId;
-        vm.messages = [];
-
+        vm.limit =  4;
+        console.log(vm.userId);
         //беру инфу о таске
         $http.post(envService.read('apiUrl')+"/api/tasks/task",{_id:vm.taskId,userId:userId})
             .success(function(response){
@@ -14,22 +15,26 @@ export class TaskInfoController{
             .error(function(err){
                 console.log(err);
             });
+        //беру все коменты
+        $http.post(envService.read('apiUrl')+"/api/comment/all",{taskId:vm.taskId})
+            .success(function(response){
+                console.log(response);
+                vm.comments = response;
+            })
+            .error(function(err){
+                console.log(err);
+            });
 
-        vm.closeTask = function(){
-            $http.delete(envService.read('apiUrl')+"/api/close/"+vm.taskId)
+
+        vm.sendComment = function (comment) {
+            console.log(new Date(),comment);
+            var commentObj = {taskId:vm.taskId,creatorId:userId,comment:comment,createdDate:new Date()};
+            $http.post(envService.read('apiUrl')+"/api/comment/add",commentObj)
                 .success(function (res) {
-
-                })
-                .error(function (res) {
-                    console.log(err);
-                    toastr.error("Ошибка подключения","Ошибка");
-                });
-        };
-
-        vm.sendСomment = function (comment) {
-            $http.post(envService.read('apiUrl')+"/api/apiwka/"+userId,{message:message})
-                .success(function (res) {
-                    vm.messages.push(res);
+                    commentObj = res;
+                    commentObj.user = $localStorage.user;
+                    vm.comments.push(commentObj);
+                    comment = "";
                 })
                 .error(function(err){
                     console.log(err);
@@ -37,7 +42,28 @@ export class TaskInfoController{
                 });
         };
 
-        vm.logout = CheckAuthService.logout;
+         vm.removeComment = function (commentId,i) {
+                    console.log(commentId,i);
+                    $http.post(envService.read('apiUrl')+"/api/comment/delete",{_id:commentId})
+                        .success(function (res) {
+                            console.log(res);
+                            vm.comments.splice(i,1);
+                        })
+                        .error(function(err){
+                            console.log(err);
+                            toastr.error("Ошибка подключения","Ошибка");
+                        });
+                };
 
+        vm.closeTask = function(){
+            $http.delete(envService.read('apiUrl')+"/api/close/"+vm.taskId)
+                .success(function (res) {
+                    console.log(res);
+                })
+                .error(function (res) {
+                    console.log(err);
+                    toastr.error("Ошибка подключения","Ошибка");
+                });
+        };
     }
 }
